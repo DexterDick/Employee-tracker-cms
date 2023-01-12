f73b73eec4a871de088838dca97e081bb2b15670
Dexter Dick <@dexterdick@hotmail.com>
2023-01-11 19:15:59 -0500

    updated addEmployee to save employee to database

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ function menu() {
                 case "Add a role":
                     addRole();
                     break;
+                case "Add an employee":
+                    AddEmployee();
+                    break;
+                case "Update an employee role":
+                    updateRole();
+                    break;
                 default:
                 // code block
             }
@@ -90,7 +96,72 @@ function viewEmployees() {
     });
 }
 
-function AddEmployee() {}
+function AddEmployee() {
+    db.query(` select  title, id from role`, (err, result) => {
+        db.query(
+            `select * from employee where manager_id is null;`,
+            (err, manager) => {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            name: "empFirstName",
+                            message: "What is the employee's first name?",
+                        },
+                        {
+                            type: "input",
+                            name: "empLastName",
+                            message: "What is the employee's last name?",
+                        },
+                        {
+                            type: "list",
+                            name: "empRole",
+                            message: "What is the employee's role?",
+                            choices: result.map((data) => ({
+                                name: data.title,
+                                value: data.id,
+                            })),
+                        },
+                        {
+                            type: "list",
+                            name: "empManager",
+                            message: "Who is the employee's manager?",
+                            choices: manager.map((data) => ({
+                                name: data.first_name + " " + data.last_name,
+                                value: data.id,
+                            })),
+                        },
+                    ])
+                    .then(
+                        ({
+                            empFirstName,
+                            empLastName,
+                            empRole,
+                            empManager,
+                        }) => {
+                            const params = [
+                                empFirstName,
+                                empLastName,
+                                empRole,
+                                empManager,
+                            ];
+
+                            console.log(params);
+                            const sql =
+                                "INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)";
+
+                            db.query(sql, params, (err, results) => {
+                                if (err) throw err;
+                                console.log(`Added ${params} to the database`);
+                                console.table(results);
+                                menu();
+                            });
+                        }
+                    );
+            }
+        );
+    });
+}
 
 function addRole() {
     db.query(`SELECT id, name FROM department`, (err, result) => {
@@ -150,3 +221,5 @@ function addDepartment() {
             });
         });
 }
+
+function updateRole() {}
